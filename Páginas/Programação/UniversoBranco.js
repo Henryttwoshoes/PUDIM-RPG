a2cc20d62a0e56c7227116d9f5e8c2706746b770
Henrique de Castro Santos <henryttwoshoes@gmail.com>
2025-06-20 11:47:41 -0300

    finalizei o primeiro site
    Adicionei as funções de scrollto para os botões do nav, permitindo uma navegação mais fluída e bonita.
    
    Retoquei o texto do site e mudei o link do iframe para o novo link oficial do episódio.

---
+++ Páginas/Programação/UniversoBranco.js
@@ -0,0 +1,10 @@
+function scrollToSection1() {
+  document.getElementById("sinopse").scrollIntoView({ behavior: "smooth" });
+}
+
+function scrollToSection2() {
+  document.getElementById("sobre").scrollIntoView({ behavior: "smooth" });
+}
+function scrollToSection3() {
+  document.getElementById("elenco").scrollIntoView({ behavior: "smooth" });
+}
